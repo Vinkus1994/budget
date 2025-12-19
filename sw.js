47d4c05302fd404e31efbda48fc89162ebf5fec9
Vinkus1994 <vince.mira03@gmail.com>
2025-12-19 19:52:34 +0100

    Update service worker cache version to v40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budget-cache-v32";
+const CACHE_NAME = "budget-cache-v40";
 const ASSETS = [
   "./",
   "./index.html",
@@ -34,6 +34,7 @@ self.addEventListener("fetch", (event) => {
     }).catch(() => cached))
   );
 });
+
 
 
 
